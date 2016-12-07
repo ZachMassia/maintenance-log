29bf82a9c8eb984a12852e414a027a5e35496954
Zach Massia <zmassia@gmail.com>
2016-12-07 17:15:26 -0500

    Add joined-table inheritance
    This currently introduces a bug on the client side when clicking on a unit to view the detailed page. Each unit in effect has two IDs.

--- back-end/models.py
+++ back-end/models.py
@@ -2,26 +2,45 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Tractor(db.Model):
-    id                 = db.Column(db.Integer, primary_key=True)
-    unit_num           = db.Column(db.Unicode, unique=True)
+class Unit(db.Model):
+    id       = db.Column(db.Integer, primary_key=True)
+    type     = db.Column(db.Unicode)
+    unit_num = db.Column(db.Unicode, unique=True)
+
+    # If any of the default intervals are overridden, they will be referenced here.
+    intervals = db.relationship('IntervalOverride', backref="unit")
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'unit',
+        'polymorphic_on': type
+    }
+
+
+class Tractor(Unit):
+    id                 = db.Column(db.Integer, db.ForeignKey('unit.id'), primary_key=True)
     a_pm_date          = db.Column(db.Date, nullable=True)
     b_pm_km_until_next = db.Column(db.Integer)
     safety_date        = db.Column(db.Date, nullable=True)
 
+    __mapper_args__ = {
+        'polymorphic_identity': 'tractor',
+    }
 
-class Trailer(db.Model):
-    id             = db.Column(db.Integer, primary_key=True)
-    unit_num       = db.Column(db.Unicode, unique=True)
+
+class Trailer(Unit):
+    id             = db.Column(db.Integer, db.ForeignKey('unit.id'), primary_key=True)
     t_pm_date      = db.Column(db.Date, nullable=True)
     safety_date    = db.Column(db.Date, nullable=True)
     one_year_date  = db.Column(db.Date, nullable=True)
     five_year_date = db.Column(db.Date, nullable=True)
 
+    __mapper_args__ = {
+        'polymorphic_identity': 'trailer',
+    }
 
-class Truck(db.Model):
-    id             = db.Column(db.Integer, primary_key=True)
-    unit_num       = db.Column(db.Unicode, unique=True)
+
+class Truck(Unit):
+    id             = db.Column(db.Integer, db.ForeignKey('unit.id'), primary_key=True)
     a_pm_date      = db.Column(db.Date, nullable=True)
     b_pm_date      = db.Column(db.Date, nullable=True)
     safety_date    = db.Column(db.Date, nullable=True)
@@ -29,6 +48,10 @@ class Truck(db.Model):
     five_year_date = db.Column(db.Date, nullable=True)
     oil_cal_date   = db.Column(db.Date, nullable=True)
     gas_cal_date   = db.Column(db.Date, nullable=True)
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'truck',
+    }
 
 
 class DefaultInterval(db.Model):
@@ -42,5 +65,5 @@ class IntervalOverride(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     unit_type = db.Column(db.Unicode)
     service_type = db.Column(db.Unicode)
-    # unit_id = TODO: Find way to reference ID from any table, or create single "Unit table"
     interval = db.Column(db.Integer)
+    unit_id = db.Column(db.Integer, db.ForeignKey('unit.id'))
