a7004da83a42f734a24b74154aeedc0dab03d6f3
Zach Massia <zmassia@gmail.com>
2016-12-30 15:16:09 -0500

    Add calibration file parser

---
+++ back-end/calibrations_parser.py
@@ -0,0 +1,58 @@
+from itertools import islice
+from datetime import datetime
+
+import openpyxl as xl
+
+
+SHEET_NAME = 'Sheet1'
+START_ROW = 2
+
+COLUMNS = {
+    'unit_num': 0,
+    'gas_cal_date': 5,
+    'oil_cal_date': 6,
+    'propane_cal_date': 7
+}
+
+
+def filter_invalid_data(units):
+    """Make sure any empty or invalid cells have the value None.
+
+    Also returns the data as a dict of unit_num -> unit_dict.
+    """
+    filtered_units = {}
+
+    for unit in units:
+        x = {}
+        for column_name, value in unit.items():
+            if column_name == 'unit_num':
+                # Convert the unit num to a string.
+                x[column_name] = str(value)
+            elif type(value) is datetime:
+                # Convert the datetime object to a date object.
+                x[column_name] = value.date()
+            else:
+                x[column_name] = None
+
+        filtered_units[x['unit_num']] = x
+    return filtered_units
+
+
+def parse(wb):
+    sheet = wb[SHEET_NAME]
+
+    units = []
+
+    for row in islice(sheet.rows, START_ROW, None):
+        unit = {}
+
+        unit_num = row[COLUMNS['unit_num']].value
+        if not unit_num:
+            # Skip empty row
+            continue
+
+        for field_name, index in COLUMNS.items():
+            unit[field_name] = row[index].value
+        units.append(unit)
+
+    return filter_invalid_data(units)
