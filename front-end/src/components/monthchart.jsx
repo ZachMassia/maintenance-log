545b9332dea7915685962bdce9af664ecbf95209
Zach Massia <zmassia@gmail.com>
2017-03-26 21:35:23 -0400

    Convert MonthChart component to functional style

--- front-end/src/components/monthchart.jsx
+++ front-end/src/components/monthchart.jsx
@@ -1,23 +1,21 @@
-import React, { PropTypes, Component } from 'react';
+import React, { PropTypes } from 'react';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
-export default class MonthChart extends Component {
-  static propTypes = {
-    data: PropTypes.array.isRequired
-  }
 
-  render() {
-    return (
-      <BarChart width={1024} height={375} data={this.props.data}>
-        <XAxis dataKey="month" />
-        <YAxis />
-        <CartesianGrid strokeDasharray="3 3" />
-        <Tooltip />
-        <Legend />
-        <Bar dataKey="VK" stackId="a" fill="#236A62" />
-        <Bar dataKey="IP UC" fill="#3F3075" />
-        <Bar dataKey="Safety" stackId="a" fill="#80A035" />
-      </BarChart>
-    );
-  }
-}
+const MonthChart = ({ data }) =>
+  <BarChart width={1024} height={375} data={data}>
+    <XAxis dataKey="month" />
+    <YAxis />
+    <CartesianGrid strokeDasharray="3 3" />
+    <Tooltip />
+    <Legend />
+    <Bar dataKey="VK" stackId="a" fill="#236A62" />
+    <Bar dataKey="IP UC" fill="#3F3075" />
+    <Bar dataKey="Safety" stackId="a" fill="#80A035" />
+  </BarChart>;
+
+MonthChart.propTypes = {
+  data: PropTypes.array.isRequired
+};
+
+export default MonthChart;
